928dad0576d6d92f43d5716b5fd84fe0e9b9f560
apichairattana111 <apichairattana111@gmail.com>
2020-04-07 15:18:04 +0700

    dfsd

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const database = require('../config/database');
 var queryize = require('queryize');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
