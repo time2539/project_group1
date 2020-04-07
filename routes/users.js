45e11c98818f7a0afcab8ae319e992f527d00347
apichairattana111 <apichairattana111@gmail.com>
2020-04-07 15:11:32 +0700

    stag

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var queryize = require('queryize');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/userProfile', function(req, res, next) {
+  var query = select()
+  .from('users','u')
+  .where({'u.user_id': 1})
+  .compile()
+  await connection.end();
+  res.send(query);
+});
+
+router.post('/updateProfile', function(req, res, next) {
+  var query = select();
+  res.send('respond with a resource');
+});
+
 
 module.exports = router;
