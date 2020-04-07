b22eff469cb1bceac454127d1dff1fdb03d6e109
apichairattana111 <apichairattana111@gmail.com>
2020-04-07 14:45:38 +0700

    update

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var usersRouter = require('./routes/users'); //user path
+var cors = require('cors')
 var app = express();
+var bodyParser = require('body-parser');
+var testRouter = require('./routes/test');
 
 // view engine setup
+app.use(bodyParser.urlencoded({
+  limit: '500mb'
+  ,extended: true
+  ,parameterLimit: 100000000000000 
+})
+);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -18,9 +27,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test',testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +49,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2/promise');
+const database = require('../config/database');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/test',async function(req, res, next){
+  const connection = await mysql.createConnection(database)
+  let sql = 'select * from user'
+  const [row] = await connection.query(sql)
+  await connection.end()
+  res.send(row);
 });
 
 module.exports = router;
